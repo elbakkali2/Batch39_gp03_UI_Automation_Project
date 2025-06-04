32befe9968bb3c88760086453a145b13e541ceb8
melisa <karakoyuncumelisa@gmail.com>
2025-06-04 15:01:34 -0500

    Created login method

---
+++ src/test/java/com/project_name/pages/LoginPage.java
@@ -0,0 +1,30 @@
+package com.project_name.pages;
+
+import com.project_name.utilities.Driver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+    public LoginPage() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(id = "login")
+    public WebElement emailInput;
+
+    @FindBy(id = "password")
+    public WebElement passwordInput;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement submitButton;
+
+
+    public void login(String email, String password){
+        this.emailInput.sendKeys(email);
+        this.passwordInput.sendKeys(password);
+        this.submitButton.click();
+    }
+
+}
