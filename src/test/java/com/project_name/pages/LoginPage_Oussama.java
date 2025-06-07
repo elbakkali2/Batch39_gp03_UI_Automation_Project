70a499b64da189971cfba86d660fbbb87177f7ce
elbakkali2 <your_no_reply_email@users.noreply.github.com>
2025-06-07 14:13:57 -0400

    i created my own page and updated mu user story

---
+++ src/test/java/com/project_name/pages/LoginPage_Oussama.java
@@ -0,0 +1,66 @@
+package com.project_name.pages;
+
+import com.project_name.utilities.ConfigurationReader;
+import com.project_name.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class LoginPage_Oussama {
+
+    public LoginPage_Oussama() {
+        PageFactory.initElements(Driver.getDriver(), this);
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(submitButton));
+    }
+
+    @FindBy(id = "login")
+    public WebElement emailInput;
+
+    @FindBy(id = "password")
+    public WebElement passwordInput;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement submitButton;
+
+    @FindBy(xpath = "//*[contains(text(), 'Wrong login/password')]")
+    public WebElement wrongLoginPassword;
+
+    @FindBy(xpath = "//li[contains(@class,'o_user_menu')]")
+    public WebElement userMenu;
+
+    @FindBy(xpath = "(//a[@href='#'])[7]")
+    public WebElement logout;
+
+    public void login(String email, String password){
+        emailInput.clear();
+        emailInput.sendKeys(email);
+        passwordInput.clear();
+        passwordInput.sendKeys(password);
+    }
+
+    public void clickSubmit() {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(submitButton));
+        submitButton.click();
+    }
+
+    public void loginAs(String role) {
+        String formattedRole = role.toLowerCase().replace(" ", "_");
+        String username = ConfigurationReader.getProperty(formattedRole + "_username");
+        String password = ConfigurationReader.getProperty(formattedRole + "_password");
+
+        if (username == null || password == null) {
+            throw new IllegalArgumentException("Missing credentials for role: " + role);
+        }
+
+        login(username, password);
+    }
+
+}
+
+
