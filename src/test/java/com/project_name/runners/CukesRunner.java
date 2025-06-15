ea5c7e290af571b6cc7196003b10da05e3b2d861
mrter <mr.terekh@gmail.com>
2025-06-15 11:27:34 -0400

    Commit 10 - edit CukesRunner

--- src/test/java/com/project_name/runners/CukesRunner.java
+++ src/test/java/com/project_name/runners/CukesRunner.java
@@ -6,9 +6,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"html:target/cucumber-reports.html",
+        plugin = {
+                "html:target/cucumber-reports.html",
                 "rerun:target/rerun.txt",
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber"},
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
         features = "src/test/resources/features",
         glue = "com/project_name/step_definitions",
   
